7ea9c06da8b4799b581839fc7b3a55493e53c939
clys-man <clysmanalves@gmail.com>
2025-04-25 10:18:17 -0300

    feat: add default configurations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,58 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configurePasswordValidation();
+        $this->configureDates();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! app()->isProduction());
+        Model::unguard();
+    }
+
+    /**
+     * Configure the password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn() => app()->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }
